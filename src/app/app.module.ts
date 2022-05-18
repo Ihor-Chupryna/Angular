811441a1ed48c457a12f7d89b01af5e56464d622
ihor <igchup1985@gmail.com>
2022-05-18 15:25:14 +0300

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CommentComponent } from './services/comment/comment.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentDetailsComponent } from './components/comment-details/comment-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommentComponent
+    CommentComponent,
+    HeaderComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent,
+    CommentsComponent,
+    CommentDetailsComponent
   ],
   imports: [
     BrowserModule
