3bc91334d71bed41d270fd4aa83db7555f272abd
ihor <igchup1985@gmail.com>
2022-06-02 00:35:43 +0300

    behaviorSubject

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+import {IUser} from "../interfaces";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  userStorage = new BehaviorSubject<IUser>({id: 0, name: '', username: '', email: ''})
+
+  constructor() {
+  }
+}
